d4599272bca74fa8b073eadbbc6d3dcedd467591
Lorenzo Grossi <126617462+onetrainn@users.noreply.github.com>
2023-04-28 18:47:49 +0200

    Add files via upload

---
+++ get_next_def/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgrossi <lgrossi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 14:03:21 by mcenci            #+#    #+#             */
+/*   Updated: 2023/04/18 14:33:36 by lgrossi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE
+#endif
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+char 	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+#endif
